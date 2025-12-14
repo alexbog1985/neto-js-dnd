6a24b9fed0c4b64dc58118933b9c1fab4232f9ad
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2025-12-14 11:04:56 +0300

    готовое окружение

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,11 +1,7 @@
 import demo from "./app";
 
 describe("Пример теста", () => {
-  test.each([
-    { str: "Hello!", expected: "Demo: Hello!" },
-    { str: "", expected: "Demo: " },
-    { str: 100, expected: "Demo: 100" },
-  ])("demo($str)", ({ str, expected }) => {
-    expect(demo(str)).toBe(expected);
-  });
+  test('should be true', () => {
+    expect(true).toBe(true);
+  })
 });
